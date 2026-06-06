0973cc5e9ba7401c069ed566bfbeee4c293b9bf6
dominguezpozodiana-sketch <dominguezpozodiana@gmail.com>
2026-06-06 18:23:20 -0400

    julo2

--- app.py
+++ app.py
@@ -30,6 +30,21 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+    dueno = Usuario.query.filter_by(
+        telefono="5351643042"
+    ).first()
+
+    if not dueno:
+        dueno = Usuario(
+            nombre="Creador",
+            telefono="5351643042",
+            rol="dueno"
+        )
+
+        dueno.set_password("Admin1234")
+        db.session.add(dueno)
+        db.session.commit()
+
 
 from routes.auth import auth_bp
 from routes.jugadas import jugadas_bp
